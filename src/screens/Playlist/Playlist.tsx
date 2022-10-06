7bc2d97c72c72d3dee46ab6fdab12a7cea4fa40d
Montso-Matlaletsa <montso.matlaletsa@gmail.com>
2022-10-06 22:08:04 +0200

    chore: add footer player

--- src/screens/Playlist/Playlist.tsx
+++ src/screens/Playlist/Playlist.tsx
@@ -1,10 +1,28 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { colors } from "../../util/colors";
 
 const Playlist = () => {
   return (
-    <View>
-      <Text>Playlist</Text>
+    <View
+      style={{
+        height: "100%",
+        marginRight: 10,
+        marginLeft: 10,
+      }}
+    >
+      <View
+        style={{
+          height: 50,
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          flexDirection: "row",
+          marginBottom: 10,
+          backgroundColor: colors.orange,
+          borderRadius: 5,
+        }}
+      ></View>
     </View>
   );
 };
