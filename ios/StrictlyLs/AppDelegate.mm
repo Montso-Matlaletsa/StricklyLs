6f9641b96ed2f822b4a41fdd108056d996fd7fdc
Montso-Matlaletsa <montso.matlaletsa@gmail.com>
2023-05-29 15:21:25 +0200

    add app center

--- ios/StrictlyLs/AppDelegate.mm
+++ ios/StrictlyLs/AppDelegate.mm
@@ -6,6 +6,10 @@
 
 #import <React/RCTAppSetupUtils.h>
 
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -57,6 +61,10 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
   return YES;
 }
 
